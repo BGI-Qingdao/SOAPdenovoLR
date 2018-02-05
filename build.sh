4583b4515d400e78d34e9a1434b8cfb9f1b42e81
Guo Lidong <guolidong@genomics.cn>
2018-02-05 15:14:13 +0800

    compile script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 tag=`date +"%Y-%m-%d_%H:%M:%S"`
-rename SOAPdenovo "SOAPdenovo_"$tag"_" *
+rename SOAPdenovo "SOAPold_"$tag"_" *
 make clean all
 make debug=1 clean all
 make 127mer=1 clean all
